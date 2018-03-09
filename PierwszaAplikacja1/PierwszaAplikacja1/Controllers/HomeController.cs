fb7712aa08eab5d005d4e526ee8cbfc270dfe40e
unknown <grzegorz.maksim@gmail.com>
2018-03-09 12:02:40 +0100

    first commit

---
+++ PierwszaAplikacja1/PierwszaAplikacja1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PierwszaAplikacja1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PierwszaAplikacja1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.Test = "Testowa wartosc";
+            var kontakty = new List<Kontakt> { new Kontakt { Imie="Jan", Nazwisko="Nowak",Miasto="Łeczna",Ulica="Mickiewicza"},
+            new Kontakt{Imie="Grzegorz",Nazwisko="Maksim",Miasto="Lublin",Ulica="Biskupie Kolonia"}};
+            ViewBag.Kontakty = kontakty;
+            return View();
+        }
+        //public string Index(string id)
+        //{
+        //    return "kurs ASP,NET MVC"+id;
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PierwszaAplikacja1/PierwszaAplikacja1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PierwszaAplikacja1.MvcApplication" Language="C#" %>
